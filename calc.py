227726068604290222ebdee0b206ef868875107b
Hyrge <ekek7478@gmail.com>
2024-10-07 01:25:38 +0900

    feat:나눗셈 추가

--- calc.py
+++ calc.py
@@ -9,12 +9,19 @@ def subtract(x, y):
 def multiply(x, y):
     return x * y 
 
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero."
+    else:
+        return x / y
+
+
 print("1. 덧셈")
 print("2. 뺄셈")
 print("3. 곱셈")
+print("4. 나눗셈")
 
-
-choice = input("원하는 연산을 선택하세요 (1/2/3): ")
+choice = input("원하는 연산을 선택하세요 (1/2/3/4): ")
 
 num1 = float(input("첫 번째 숫자를 입력하세요: "))
 num2 = float(input("두 번째 숫자를 입력하세요: "))
@@ -23,7 +30,11 @@ if choice == '1':
     print(f"{num1} + {num2} = {add(num1, num2)}")
 elif choice == '2':
     print(f"{num1} - {num2} = {subtract(num1, num2)}")
-else :
+elif choice == '3':
     print(f"{num1} * {num2} = {multiply(num1, num2)}")
+elif choice == '4':
+    print(f"{num1} / {num2} = {divide(num1, num2)}")
+else:
+    print("잘못된 입력입니다.")
 
 
